8aefb60c39e4bdee609203f63a85b49b9a690b2e
James Chao <jameschao.lapion@gmail.com>
2022-04-06 22:31:34 -0700

    FirstProgram

---
+++ Week1/Extra Practice/FirstProgram.java
@@ -0,0 +1,5 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is James" + '\n' + "Im 27 years old" + '\n' + "I was born and raised in Seattle!");
+    }
+}
