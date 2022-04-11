d1814cdadb8785732859081a0f88c69b3f9d1edf
James Chao <jameschao.lapion@gmail.com>
2022-04-10 17:35:48 -0700

    Completed 4/10

--- Week1/Cafe Business Logic/CafeUtil.java
+++ Week1/Cafe Business Logic/CafeUtil.java
@@ -1,10 +1,47 @@
+import java.util.ArrayList;
+
+
 public class CafeUtil{
 
+    //Sum goal 
     public int getStreakGoal(int numweeks){
         int goal = 0;
-        for(int i = 0; i == numweeks; i++){
+        for(int i = 0; i <= numweeks; i++){
             goal = goal + i;
         }
         return goal;
+    }
+
+    // Sum prices in double array
+    public double getOrderTotal(double[]prices){
+        double total = 0;
+        for(double price : prices){
+            total = total + price;
+        }
+        return total;
+    }
+
+    //display menu
+    public void displayMenu(ArrayList<String>menuItems){
+        System.out.println(menuItems.toString());
+    }
+
+    //adding customer input and displaying list of customers in line
+    public void addCustomer(ArrayList<String>customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.printf("Hello %s, ", userName);
+        System.out.printf("there are %x people in front of you\n", customers.size());
+        customers.add(userName);
+        System.out.println(customers.toString());
+    }
+
+    //print price for product up to max + the Sensei challenge
+    public void printPriceChart(String product, double price, int maxQuantity){
+        System.out.printf("%s", product);
+        for(int i = 1; i <= maxQuantity; i++){
+            System.out.printf("\n%s : $%.2f", i, ((i * price) - ((i - 1) * .50)));
+        }
+        System.out.println("\n");
     }
 }
--- Week1/Cafe Business Logic/TestCafe.java
+++ Week1/Cafe Business Logic/TestCafe.java
@@ -8,34 +8,38 @@ public class TestCafe {
     of the CafeUtil class. 
     Hint: it will need to correspond with the variable name used below..
     */
-        CafeUtil cafeUtil = new CafeUtil();
+        CafeUtil appTest = new CafeUtil();
 
-        cafeUtil
+
     
         /* ============ App Test Cases ============= */
     
         System.out.println("\n----- Streak Goal Test -----");
-        System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
+        System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal(10));
     
-        // System.out.println("----- Order Total Test-----");
-        // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
-        // System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
         
-        // System.out.println("----- Display Menu Test-----");
+        System.out.println("----- Display Menu Test-----");
         
-        // ArrayList<String> menu = new ArrayList<String>();
-        // menu.add("drip coffee");
-        // menu.add("cappuccino");
-        // menu.add("latte");
-        // menu.add("mocha");
-        // appTest.displayMenu(menu);
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        appTest.displayMenu(menu);
     
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
-        // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     appTest.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            appTest.addCustomer(customers);
+            System.out.println("\n");
+        }
+
+        System.out.println("\n-----Quantity Prices Test-----");
+        appTest.printPriceChart("Frappe",4.5, 3);
+        appTest.printPriceChart("Dark Coffee", 3.5, 2);
     }
 }
