da1ee05235b857739e7c60bc14c4b15ade77d57d
James Chao <jameschao.lapion@gmail.com>
2022-04-20 22:23:39 -0700

    commit 4/20

---
+++ Week3/daikichiroutes/src/test/java/com/james/daikichi/DaikichiroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
