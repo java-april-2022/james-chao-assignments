66c58b21e8320e6eff546b6ed78fe2519764d0fd
James Chao <jameschao.lapion@gmail.com>
2022-04-22 22:53:37 -0700

    commit 4/22

---
+++ Week3/Daikichi Path Variables/src/main/java/com/james/daikichi/controllers/DaikichiController.java
@@ -0,0 +1,22 @@
+package com.james.daikichi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DaikichiController {
+	@RequestMapping("/daikichi")
+	 public String index() {
+		 return "Welcome";
+	 }
+	
+	@RequestMapping("/daikichi/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors";
+	}
+	
+	@RequestMapping("/daikichi/tomorrow")
+	public String tomorrow(){
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+}
